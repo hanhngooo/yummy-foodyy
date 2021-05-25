a3f8f51cc251f39c80302f4bc757932501f80357
hanhngooo <hanhngo1795@gmail.com>
2021-05-25 23:20:14 +0200

    set env api key, try out an api call

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,31 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
+import { apiUrl } from '@/utility/constant'
 
 export default Vue.extend({
   name: 'Home',
-  components: {}
+  components: {},
+  data() {
+    return {
+      fetchedData: 'haha'
+    }
+  },
+  methods: {
+    async getData(): Promise<void> {
+      try {
+        const response = await axios.get(
+          `${apiUrl}/random?apiKey=${process.env.VUE_APP_API_KEY}`
+        )
+        console.log('reposne', response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  mounted() {
+    this.getData()
+  }
 })
 </script>
