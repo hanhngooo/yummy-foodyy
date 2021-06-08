15ed80e64c258816340e0bef294446bfa14f4f32
hanhngooo <hanhngo1795@gmail.com>
2021-06-09 00:52:18 +0200

    add sitemap

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pluginOptions: {
+    sitemap: {
+      urls: [
+        'https://yummy-foody.netlify.app/',
+        'https://yummy-foody.netlify.app/categories/asian',
+        'https://yummy-foody.netlify.app/categories/dinner',
+        'https://yummy-foody.netlify.app/categories/dessert',
+        'https://yummy-foody.netlify.app/categories/pasta',
+        'https://yummy-foody.netlify.app/categories/drinks',
+        'https://yummy-foody.netlify.app/today-recipe',
+        'https://spoonacular.com/food-api/docs'
+      ]
+    }
+  }
+}
