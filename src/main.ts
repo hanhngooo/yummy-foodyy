511bbf676b1353b52bf19208355515c6b997330d
hanhngooo <hanhngo1795@gmail.com>
2021-06-10 11:18:13 +0200

    remove store

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import './assets/globalstyle.css'
 import { BootstrapVue } from 'bootstrap-vue'
 
@@ -13,6 +12,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
